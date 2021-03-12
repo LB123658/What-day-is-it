96dbff98db2c6ff5e3e6f695f39102e1c1f9a6bf
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-03-12 13:43:47 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var phrase = document.getElementById("phrase");
+  var d = new Date();
+  var n = d.getDay()
+  console.log = n;
+if (console.log == 5) {
+phrase.innerHTML = "Happy Friday!";
+} else if (console.log == 1) {
+phrase.innerHTML = "Happy Monday!";
+}  else if (console.log == 2) {
+phrase.innerHTML = " Happy Tuesday!";
+} else if (console.log == 3) {
+phrase.innerHTML = "Happy Wednesday!";
+} else if (console.log == 4) {
+phrase.innerHTML = "Happy Thursday!";
+} else if (console.log == 6) {
+phrase.innerHTML = "Happy Saturday!";
+} else if (console.log == 7) {
+phrase.innerHTML = "Happy Sunday!";
+}
